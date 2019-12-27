4ca56a9ce753c249bcaf81a88e4cb8fd9e7e086d
ivannabura <ibura001@gmail.com>
2019-12-27 11:31:24 +0200

    fr commit 6

--- db_create.js
+++ db_create.js
@@ -3,6 +3,7 @@ const DB_PATH = 'mydatabase.db'
 const DB = new sqlite3.Database(DB_PATH) 
 
 DB.serialize(() => {
+    
     DB.run(
         'CREATE TABLE IF NOT EXISTS Kids (id INTEGER PRIMARY KEY UNIQUE, thing TEXT, price TEXT, customer TEXT)'
     )
